ee1cbcc53f02ba8f08a487ea2ae169e62750033d
akshaymurali01 <akshaymurali01+github@gmail.com>
2022-01-09 20:32:04 +0530

    Q2

---
+++ Q2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int max(int a,int b)
+{
+    return (a>b)?a:b;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int x[n];
+    int dp[n+1];
+    for(int i=0;i<n;i++)
+    {
+        cin>>x[i];
+    }
+    dp[0] = 0;
+    dp[1] = x[n-1];
+    dp[2] = max(x[n-1],x[n-2]);
+    for(int i=3;i<n+1;i++)
+    {
+        dp[i] = max(dp[i-1],x[n-i]+dp[i-2]);
+    }
+    cout<<dp[n];
+}
